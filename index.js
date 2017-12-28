0f696a7967dde7f97d413a5ec3ab83754440a9c7
MarcoXZh3 <zxu3@ualberta.ca>
2017-12-28 12:34:06 -0700

    Version1.1.1: database and user changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Main entry of the steemit blog autobot
  * @author  MarcoXZh3
- * @version 1.0.1
+ * @version 1.1.1
  */
 const CronJob = require('cron').CronJob;
 const encryption = require('./libencryption');
