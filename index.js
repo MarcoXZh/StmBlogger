7507612b93aa5b57a2931c6671121ac486dc82a9
MarcoXZh3 <zxu3@ualberta.ca>
2017-12-27 20:43:48 -0700

    Version 1.0.1: change the schedule to run every day

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Main entry of the steemit blog autobot
  * @author  MarcoXZh3
- * @version 1.0.0
+ * @version 1.0.1
  */
 const CronJob = require('cron').CronJob;
 const encryption = require('./libencryption');
@@ -15,13 +15,13 @@ var options = JSON.parse(fs.readFileSync('options.json', 'utf8').toString());
 options.author.posting = keys.posting;
 options.db.uri = 'mongodb://' + options.db.user + ':' + keys.dbkey
                               + '@localhost:27017/' + options.db.name;
+var now = new Date(new Date().getTime() + 3000);
+var cron = now.getUTCSeconds() + ' ' + now.getUTCMinutes() + ' '
+         + now.getUTCHours() + ' * * *';
 
 // The job to query CN tags
-new CronJob((new Date().getUTCSeconds() + 2) + ' * * * * *', function() {
+new CronJob(cron, function() {
     queryCNTags(options, function(blog) {
         // Do nothing
     }); // queryCNTags(options, function(blog) { ... });
-}, null, true, 'UTC'); // new CronJob( ... );
-
-
-
+}, null, true, 'UTC'); // new CronJob(cron, function() { ... }, ... );
